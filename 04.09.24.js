c5ecfa8a4404ba09a9edb0239507f778e8a38027
Nadia <148998471+NadiaSuganiaka@users.noreply.github.com>
2024-09-11 21:01:42 +0300

    Update 04.09.24.js

--- 04.09.24.js
+++ 04.09.24.js
@@ -1,24 +1,29 @@
-function binary(arr, number) {
+function binarySearch(arr, desiredNumber) {
     let left = 0; 
     let right = arr.length -1;
 
     while (left <= right){
         let mid = Math.floor((left + right) / 2);
-        if (arr[mid] == number) {
+        if (arr[mid] === desiredNumber) {
             return mid;
-        } else if (arr[mid] < number){
-            left = mid;
+        } else if (arr[mid] < desiredNumber) {
+            left = mid + 1;
         } else {
-            right = mid;
+            right = mid - 1;
         }
     }
+    
+    return -1;
 }
-function linear(arr, number) {
+
+function linearSearch(arr, desiredNumber) {
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == number) {
+        if (arr[i] === desiredNumber) {
             return i;
         }
     }
+    
+    return -1;
 }
 
 const arr= [
@@ -28,12 +33,11 @@ const arr= [
     27, 28, 29, 30, 31, 32, 33, 34
 ];
 
-let number = 22;
+const desiredNumber = 22;
 
-let resLinear = linear(arr, number);
-let resBinary = binary(arr, number);
+const resLinear = linearSearch(arr, desiredNumber);
+const resBinary = binarySearch(arr, desiredNumber);
 
 
-console.log("Linear: ", arr[resLinear]);
-console.log("Binary: ", arr[resBinary]);
-
+console.log("Linear: ", resLinear !== -1 ? arr[resLinear] : 'Not found');
+console.log("Binary: ", resBinary !== -1 ? arr[resBinary] : 'Not found');
